fe11c2ae11aa23ee9ffbac7587eab62a93e860c3
Chris <scapenz@gmail.com>
2019-04-30 18:03:13 +1200

    on load event

---
+++ content.js
@@ -0,0 +1,20 @@
+$(window).on('load', function() {
+
+// content.js
+var targetList = [
+  ['premium', ''],
+  ['premium-content', 'content'],
+  ['article-offer', 'hidden'],
+  ['premium-sub', '']
+];
+
+var els = [];
+
+for (var i=0; i<targetList.length; i++)
+{
+  find = targetList[i][0];
+  replace = targetList[i][1];
+  $('.' + find).addClass(replace).removeClass(find);
+}
+
+}
